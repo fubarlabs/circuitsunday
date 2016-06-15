3c8ede4cfd5873bab3a7af007598209b8b41df84
Ricklon <rick.rickanderson@gmail.com>
2016-06-15 10:54:34 -0400

    Fixed the missing braces. Added MOSI, MISO definitions.

--- session5/DotstarMinLoop/DotstarMinLoop.ino
+++ session5/DotstarMinLoop/DotstarMinLoop.ino
@@ -3,18 +3,17 @@
 #include <SPI.h>         //comment out for softwareSPI
 //#include <SoftSPI.h> //uncomment to use non standard pins
 
-#define DATAPIN    25
-#define CLOCKPIN   26
+#define DATAPIN    MISO
+#define CLOCKPIN   MOSI
 #define NUMPIXELS 30 // Number of LEDs in strip
 
 #define red  0xFF0000
 #define green  0x00FF00
 #define blue  0x0000FF
 
-int upindex = 0;
-int downindex = 30;
-
 uint32_t frame[NUMPIXELS];
+
+
 uint32_t colors[3] = {green, red, blue };
 
 Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
@@ -43,31 +42,43 @@ void loop() {
     strip.show();
   */
 
-  if (upindex > NUMPIXELS)
-  {
-    upindex = 0;
+  for (int ii = 0; ii < NUMPIXELS; ii++) {
+    if (ii == 0)
+    {
+      frame[ii] = 0xFFFFFF;
+      strip.setPixelColor(ii, frame[ii]);
+      strip.show();
+      delay(100);
+    }
+    else
+    {
+      frame[ii - 1] = 0x000000;
+      frame[ii] = 0xFFFFFF;
+      strip.setPixelColor(ii - 1, frame[ii - 1]);
+      strip.setPixelColor(ii, frame[ii]);
+      strip.show();
+      delay(100);
+    }
+
+
+    for (int ii = NUMPIXELS; ii < 0; ii--) {
+      if (ii == 30)
+      {
+        frame[ii] = 0xFFFFFF;
+        strip.setPixelColor(ii, frame[ii]);
+        strip.show();
+        delay(100);
+      }
+      else
+      {
+        frame[ii + 1] = 0x000000;
+        frame[ii] = 0xFFFFFF;
+        strip.setPixelColor(ii + 1, frame[ii + 1]);
+        strip.setPixelColor(ii, frame[ii]);
+        strip.show();
+        delay(100);
+      }
+    }
   }
-  if (downindex < 0)
-  {
-    downindex = NUMPIXELS;
-  }
-  upindex++;
-  downindex--;
-
-  frame[upindex - 1] = 0x000000;
-  frame[upindex] = 0xFFFFFF;
-  strip.setPixelColor(upindex - 1, frame[upindex - 1]);
-  strip.setPixelColor(upindex, frame[upindex]);
-
-
-  frame[downindex + 1] = 0x000000;
-  frame[downindex] = 0xFFFFFF;
-  strip.setPixelColor(downindex + 1, frame[downindex + 1]);
-  strip.setPixelColor(downindex, frame[downindex]);
-
-  strip.show();
-  delay(100);
-
-
-
 }
+
